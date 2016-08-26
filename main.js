75d9a05aded355b6beaa98e57e70ef7afd98e8b3
Alexandre Braga <alexandre_cgr@hotmail.com>
2016-08-26 14:01:23 -0400

    first lines

---
+++ main.js
@@ -0,0 +1,5 @@
+var seneca = require("seneca")();
+
+seneca.add({role: "accountManagement", cmd: "login"}, function(args, respond){});
+seneca.add({role: "accountManagement", cmd: "register"}, function(args, respond){});
+
